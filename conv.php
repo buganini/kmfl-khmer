87b1440ba4fbead9cdd856f04bd0ec386446e862
Buganini <buganini@gmail.com>
2010-07-24 17:12:11 +0800

    first commit

---
+++ conv.php
@@ -0,0 +1,21 @@
+<?php
+$s=file_get_contents($argv[1]);
+$s=str_replace("\r\n","\n",rtrim($s));
+$a=explode("\n",$s);
+$b=array();
+foreach($a as $v){
+	if($v[0]=='#') continue;
+	$b[]=explode("\t",rtrim($v));
+}
+$a=$b;
+function quot($s){
+	if(strpos($s,'"')===false) return '"'.$s.'"';
+	if(strpos($s,'\'')===false) return '\''.$s.'\'';
+	die('Dont know how to quote '.$s);
+}
+for($i=1;$i<count($a);++$i){
+	for($j=1;$j<count($a[$i]);++$j){
+		if(!empty($a[$i][$j])) echo '+ ['.$a[$i][0].$a[0][$j].'] > '.quot($a[$i][$j])."\n";
+	}
+}
+?>
